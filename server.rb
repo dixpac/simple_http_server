3722356d373f65f1d5f2ab4164bd7fc417d3850d
Dino Maric <dino.onex@gmail.com>
2023-07-31 12:43:01 +0200

    Start parsing into Request object

--- server.rb
+++ server.rb
@@ -1,14 +1,39 @@
 require 'socket'
 
+class Request
+  attr_reader :method, :path, :headers, :body
+
+  def initialize(request)
+    lines = request.lines
+
+    @method, @path, = lines.first.split
+
+    index = lines.index("\r\n")
+    @headers = parse_headers(lines[1...index])
+
+    @body = lines[(index - 1)..-1].join
+  end
+
+  def parse_headers(lines)
+    headers = {}
+
+    lines.each do |line|
+      name, value = line.split(': ')
+      headers[name.downcase] = value.chomp
+    end
+
+    headers
+  end
+end
+
 port = ENV.fetch('SIMPLE_PORT', '3008').to_i
 server = TCPServer.new(port)
 
 puts "Listening on port #{port}"
-
 loop do
   client = server.accept
-  request = client.readpartial(2048)
-  puts request
+  request = Request.new client.readpartial(2048)
+  p request
 
   client.puts "HTTP/1.1 200 OK\r\n"
   client.puts "Content-Type: text/html\r\n"
