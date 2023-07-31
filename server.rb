78edf086d123a494674cda5d45375d34edc044b0
Dino Maric <dino.onex@gmail.com>
2023-07-31 12:31:51 +0200

    Start sending a reposnse

--- server.rb
+++ server.rb
@@ -3,8 +3,16 @@ require 'socket'
 port = ENV.fetch('SIMPLE_PORT', '3008').to_i
 server = TCPServer.new(port)
 
+puts "Listening on port #{port}"
+
 loop do
   client = server.accept
-  client.puts 'Yo'
+  request = client.readpartial(2048)
+  puts request
+
+  client.puts "HTTP/1.1 200 OK\r\n"
+  client.puts "Content-Type: text/html\r\n"
+  client.puts "\r\n"
+  client.puts "Current time is #{Time.now}\r\n"
   client.close
 end
